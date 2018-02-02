1b3941910a4adc3e8acc5b0befab782bfdf36ce7
Kentaro Hayashi <hayashi@clear-code.com>
2018-02-02 12:16:03 +0900

    Import some modules

---
+++ autoconfig-forcebrowserextensiondata.js
@@ -0,0 +1,7 @@
+{
+    // Force Browser Extension Data for Firefox 60 and later
+    const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+    const { Services } = Cu.import('resource://gre/modules/Services.jsm', {});
+    const { FileUtils } = Cu.import('resource://gre/modules/FileUtils.jsm', {});
+    const { OS } = Cu.import("resource://gre/modules/osfile.jsm", {});
+}
